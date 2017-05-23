54dad30053291c8919b4fa54e6e303b9e30e2525
Sergo <sergey.website.dev@gmail.com>
2017-05-23 17:27:13 +0300

    add button to top

--- js/scripts-ini.js
+++ js/scripts-ini.js
@@ -8,3 +8,16 @@ jQuery(function($){
     }
   });
 });
+
+jQuery(function() {
+jQuery(window).scroll(function() {
+if(jQuery(this).scrollTop() != 0) {
+jQuery('.button-top').fadeIn();
+} else {
+jQuery('.button-top').fadeOut();
+}
+});
+jQuery('.button-top').click(function() {
+jQuery('body,html').animate({scrollTop:0},800);
+});
+});
