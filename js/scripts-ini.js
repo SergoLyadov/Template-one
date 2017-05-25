42962466f31dd3af957c4a431627aa7d51458199
Sergo <sergey.website.dev@gmail.com>
2017-05-25 16:22:45 +0300

    gallery

--- js/scripts-ini.js
+++ js/scripts-ini.js
@@ -42,3 +42,60 @@ player.pauseVideo();
 jQuery('#youtube_player').on('shown.bs.modal', function (e) {
 player.playVideo();
 })
+
+jQuery('.say-us-block').slick({
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+
+ jQuery(function(){
+    var jQuerygallery = jQuery('.gallery a').simpleLightbox();
+
+    jQuerygallery.on('show.simplelightbox', function(){
+      console.log('Requested for showing');
+    })
+    .on('shown.simplelightbox', function(){
+      console.log('Shown');
+    })
+    .on('close.simplelightbox', function(){
+      console.log('Requested for closing');
+    })
+    .on('closed.simplelightbox', function(){
+      console.log('Closed');
+    })
+    .on('change.simplelightbox', function(){
+      console.log('Requested for change');
+    })
+    .on('next.simplelightbox', function(){
+      console.log('Requested for next');
+    })
+    .on('prev.simplelightbox', function(){
+      console.log('Requested for prev');
+    })
+    .on('nextImageLoaded.simplelightbox', function(){
+      console.log('Next image loaded');
+    })
+    .on('prevImageLoaded.simplelightbox', function(){
+      console.log('Prev image loaded');
+    })
+    .on('changed.simplelightbox', function(){
+      console.log('Image changed');
+    })
+    .on('nextDone.simplelightbox', function(){
+      console.log('Image changed to next');
+    })
+    .on('prevDone.simplelightbox', function(){
+      console.log('Image changed to prev');
+    })
+    .on('error.simplelightbox', function(e){
+      console.log('No image found, go to the next/prev');
+      console.log(e);
+    });
+  });
+
+jQuery('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
