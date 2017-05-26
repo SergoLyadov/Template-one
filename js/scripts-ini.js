fdb4e394103061a4f42619b6e583f09532bae4a8
Sergo <sergey.website.dev@gmail.com>
2017-05-26 16:46:38 +0300

    footer mobile menu header top block

--- js/scripts-ini.js
+++ js/scripts-ini.js
@@ -1,7 +1,7 @@
 jQuery(function($){
   $(window).scroll(function(){
     var winTop = $(window).scrollTop();
-    if(winTop >= 60){
+    if(winTop >= 55){
       $("header").addClass("sticky-header");
     }else{
       $("header").removeClass("sticky-header");
@@ -99,3 +99,28 @@ jQuery('.multiple-items').slick({
   slidesToShow: 3,
   slidesToScroll: 3
 });
+
+(function($) {
+
+    // Handle click on toggle search button
+    $('#toggle-search').click(function() {
+      $('#search-form, #toggle-search').toggleClass('open');
+      return false;
+    });
+
+    // Handle click on search submit button
+    $('#search-form input[type=submit]').click(function() {
+      $('#search-form, #toggle-search').toggleClass('open');
+      return true;
+    });
+
+    // Clicking outside the search form closes it
+    $(document).click(function(event) {
+      var target = $(event.target);
+      
+      if (!target.is('#toggle-search') && !target.closest('#search-form').size()) {
+        $('#search-form, #toggle-search').removeClass('open');
+      }
+    });
+
+})(jQuery);
