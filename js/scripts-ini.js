d426a8cac6832b4753ebe224ab41514b6bd49db4
Sergo <sergey.website.dev@gmail.com>
2017-06-05 07:15:04 +0200

    mobile menu update

--- js/scripts-ini.js
+++ js/scripts-ini.js
@@ -240,7 +240,29 @@ $('.slider-nav').slick({
   focusOnSelect: true
 });
 
+$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
 
-jQuery( '#dl-menu' ).dlmenu({
-  animationClasses : { classin : 'animation-class-name', classout : 'animation-class-name' }
+    // Variables privadas
+    var links = this.el.find('.link');
+    // Evento
+    links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+  }
+
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el;
+      $this = $(this),
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+    };
+  } 
+
+  var accordion = new Accordion($('#accordion'), false);
 });
